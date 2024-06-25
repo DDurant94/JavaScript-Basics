bd2f4a54e59d8323380179c84f89275438638071
DDurant94 <dannyjdurant@gmail.com>
2024-06-25 13:11:29 -0500

    finished project

---
+++ calculator.js
@@ -0,0 +1,42 @@
+//question two
+
+let x = 6;
+let y =3;
+
+let isPositive = x > 0 && y > 0;
+
+let isEven = x % 2 === 0 || y % 2 === 0;
+
+console.log("Sum:", x + y);
+console.log("Difference:", x - y);
+console.log("Product:", x * y);
+console.log("Quotient:", x / y);
+
+console.log("Is num1 equal to num2?", x == y);
+console.log("Is num1 greater than num2?", x > y);
+console.log("Is num1 not equal to num2?", x != y);
+
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
+
+
+
+
+x = 5;
+y = 10;
+
+isPositive = x > 0 && y > 0;
+
+isEven = x % 2 === 0 || y % 2 === 0;
+
+console.log("Sum:", x + y);
+console.log("Difference:", x - y);
+console.log("Product:", x * y);
+console.log("Quotient:", x / y);
+
+console.log("Is num1 equal to num2?", x == y);
+console.log("Is num1 greater than num2?", x > y);
+console.log("Is num1 not equal to num2?", x != y);
+
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
---
+++ student_management.js
@@ -0,0 +1,21 @@
+//Question One:
+
+studentName0 = "Jim";
+studentAge0 = 15;
+studentGrade0 = "A";
+isPresent0 = false;
+
+studentName1 = "Alice";
+studentAge1 = 15;
+studentGrade1 = "B";
+isPresent1 = true;
+
+console.log ("Student Name:", studentName0)
+console.log("Age:", studentAge0)
+console.log("Grade:", studentGrade0)
+console.log(`Student Attending Today? ${isPresent0 ? 'Yes':'No'}`)
+
+console.log ("Student Name:", studentName1)
+console.log("Age:", studentAge1)
+console.log("Grade:", studentGrade1)
+console.log(`Student Attending Today? ${isPresent1 ? 'Yes':'No'}`)
